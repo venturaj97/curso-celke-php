763d1273ec18d45a2d7aba073bc5fe467132dacf
Jv <joao.mendes@camtecnologia.com.br>
2024-09-21 02:45:41 -0300

    exercicio 1 do curso de php

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,2 +1,27 @@
 <?php
 
+$nome = "João Victor";
+$sobrenome = "Ventura";
+
+echo $nome ." ". $sobrenome;
+
+echo "<br><br><br>";
+
+$salario = 1288.00;
+$profissao = "garçom";
+echo "$nome <br>";
+echo $salario;
+echo "<br>$profissao";
+
+
+/* 
+1 – Implemente um programa com duas variáveis, uma variável para receber o
+nome da pessoa e uma variável que receba o sobrenome. No final do
+programa imprima na tela o nome e o sobrenome concatenado.
+Nome do projeto – exercicio0001.php
+
+2 – Implemente um programa com tres variáveis, uma para receber o nome da
+pessoa, uma variável que receba o salário e uma variável que receba o nome
+do cargo. No final do programa imprima na tela o nome, salário e o cargo.
+Nome do projeto – exercicio0002.php
+*/
